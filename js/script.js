95c19d6f659c271eed65bb8c6339a994543aa62a
William Kang <william@wkang.ca>
2024-07-24 19:54:48 -0400

    update faq

--- js/script.js
+++ js/script.js
@@ -8,30 +8,29 @@ for (const item of accordionItems) {
 }
 
 // Set the date we're counting down to
-var countDownDate = new Date("November 2, 2024 15:37:25").getTime();
+const countDownDate = new Date("November 2, 2024").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
-
+const countdown = document.querySelector('.countdown-timer');
+const x = setInterval(() => {
   // Get today's date and time
-  var now = new Date().getTime();
+  const now = Date.now();
 
   // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+  const distance = countDownDate - now;
 
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  countdown.innerText = `${days}d ${hours}h ${minutes}m ${seconds}s`;
 
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    countdown.innerText = "";
   }
 }, 1000);
