52a78cacfb3003f2818cf9453369120386725bb7
Doubtfull <98238524+Doubtfull@users.noreply.github.com>
2024-07-25 20:40:48 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,28 +4,21 @@ for (const item of accordionItems) {
     item.addEventListener('click', () => item.classList.toggle('active'));
 }
 
-// Set the date we're counting down to
-const countDownDate = new Date("November 2, 2024").getTime();
+const targetDate = new Date("November 2, 2024").getTime();
 
-// Update the count down every 1 second
 const countdown = document.querySelector('.countdown-timer');
 const x = setInterval(() => {
-  // Get today's date and time
-  const now = Date.now();
+  const todaysDate = Date.now();
 
-  // Find the distance between now and the count down date
-  const distance = countDownDate - now;
+  const distance = targetDate - todaysDate;
 
-  // Time calculations for days, hours, minutes and seconds
   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
   const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
   countdown.innerText = `${days}d ${hours}h ${minutes}m ${seconds}s`;
 
-  // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
     countdown.innerText = "";
